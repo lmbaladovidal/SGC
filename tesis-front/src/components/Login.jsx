92d0e2db352bbe9bb5deb31ef6d0b9074ed82cbc
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2022-10-06 07:12:31 -0300

    se agrego login.css

--- tesis-front/src/components/Login.jsx
+++ tesis-front/src/components/Login.jsx
@@ -1,12 +1,13 @@
 import { Menu } from "./Menu"
 import logo from "../assets/logo-guma.svg"
+import './Login.css'
 
 
 export const Login = () => {
   return (
-    <div className="principal">
+    <div className="contenedor-login">
         <img src={logo} alt="" className="logo"/>
-        <form action="/menu">
+        <form action="/menu" className="form-login">
           <div>
             <h5>Usuario</h5>
             <input type="text" className="text"/>
